39b63b83996d0a722b140326d0400985e6f829ac
RodrigoAlbuqueque <chidesterawilda@gmail.com>
2025-11-07 09:18:54 +0800

    Add home page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { EventsScoreApp } from "@/components/EventsScoreApp";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <EventsScoreApp />
+    </main>
+  );
+}
