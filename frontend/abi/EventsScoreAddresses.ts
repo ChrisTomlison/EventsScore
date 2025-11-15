7ea30165e286075335f4e9eee99a20382c227f31
RodrigoAlbuqueque <chidesterawilda@gmail.com>
2025-11-15 20:20:47 +0800

    Add contract addresses

---
+++ frontend/abi/EventsScoreAddresses.ts
@@ -0,0 +1,8 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const EventsScoreAddresses = { 
+  "11155111": { address: "0x782e506582f69260c1dd8B48433FD11298Fe0d2B", chainId: 11155111, chainName: "sepolia" }
+};
